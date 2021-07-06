d225d207948e0e99c0cf39bff35b3074f562fac8
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-07-06 16:07:51 +0800

    drm-hotplug.sh: Speed it up
    Change-Id: I9c5188e6e8a4439d941657f25ef38507fff718d5
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- overlay/usr/local/bin/drm-hotplug.sh
+++ overlay/usr/local/bin/drm-hotplug.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -x
 
 export DISPLAY=${DISPLAY:-:0}
-export XUSER=${XUSER:-root}
 
 function prepare_env() {
-    sudo -u $XUSER xdpyinfo &>/dev/null && return
-
     # Try to figure out XAUTHORITY and DISPLAY
     for pid in $(pgrep X 2>/dev/null || \
         ls /proc|grep -ow "[0-9]*"|sort -rn); do
@@ -20,20 +17,13 @@ function prepare_env() {
         export DISPLAY=$(cat $PROC_DIR/cmdline|tr '\0' '\n'| \
             grep -w "^:.*" || echo ":0")
 
-        echo Found auth: $XAUTHORITY for dpy: $DISPLAY
-        break
+        logger -t $0 "Found auth: $XAUTHORITY for dpy: $DISPLAY"
+        return
     done
-
-    # Find an authorized user
-    for XUSER in root $(users);do
-        sudo -u $XUSER xdpyinfo &>/dev/null && return
-    done
-
-    exit 0
 }
 
 function xrandr_wrapper() {
-    sudo -u $XUSER xrandr --screen ${SCREEN:-0} $@
+    xrandr --screen ${SCREEN:-0} $@
 }
 
 function handle_monitor() {
@@ -45,16 +35,34 @@ function handle_monitor() {
     # Make sure every connected monitors been enabled with a valid mode.
     if grep -wq connected $SYS/status; then
         # Already got a valid mode
-        grep -wq "$(cat $SYS/mode)" $SYS/modes && return 0
+        grep -wq "$(cat $SYS/mode 2>/dev/null)" $SYS/modes && return 0
 
         # Ether disabled or wrongly configured
         xrandr_wrapper --output $MONITOR --auto
+
+        logger -t $0 "Output $MONITOR enabled."
     fi
 }
 
-prepare_env
+if ! xdpyinfo &>/dev/null; then
+    # Try to setup env
+    prepare_env
 
-SCREENS=$(sudo -u $XUSER xdpyinfo|grep screens|cut -d':' -f2)
+    if ! xdpyinfo &>/dev/null; then
+        # Try to switch to an authorized user
+        for XUSER in root $(users);do
+            sudo -u $XUSER xdpyinfo &>/dev/null || continue
+
+            logger -t $0 "Switch to user: $XUSER"
+            sudo -u $XUSER $0; exit 0
+        done
+
+        logger -t $0 "Unable to contact Xserver!"
+        exit 0
+    fi
+fi
+
+SCREENS=$(xdpyinfo|grep screens|cut -d':' -f2)
 for SCREEN in $(seq 0 ${SCREENS:-0}); do
     # Find monitors of screen
     MONITORS=$(xrandr_wrapper 2>/dev/null|grep connect|cut -d' ' -f1)
