c2c287dd0e7e4a171bbed22f0c9bf23bc9c96084
Caesar Wang <wxt@rock-chips.com>
2022-01-07 17:49:08 +0800

    overlay-debug: update test-camera-uvc.sh
    Change-Id: I1861d88b00c5f28767e44082dbdc0184a802f7a9
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- overlay-debug/usr/local/bin/test_camera-uvc.sh
+++ overlay-debug/usr/local/bin/test_camera-uvc.sh
@@ -6,10 +6,10 @@ export DISPLAY=:0.0
 #export GST_DEBUG_FILE=/tmp/2.txt
 #echo 600000000 > /sys/kernel/debug/clk/aclk_vcodec/clk_rate
 echo "Start UVC Camera M-JPEG Preview!"
-gst-launch-1.0 v4l2src device=/dev/video10 ! image/jpeg, width=640, height=480, framerate=30/1 ! jpegparse ! mppjpegdec ! rkximagesink sync=false
+gst-launch-1.0 v4l2src device=/dev/video17 ! image/jpeg, width=1280, height=720, framerate=30/1 ! jpegparse ! mppjpegdec ! xvimagesink sync=false
 
-#     gst-launch-1.0 v4l2src device=/dev/video10 ! video/x-raw,format=YUY2,width=640,height=480, framerate=30/1 ! videoconvert ! autovideosink
+#     gst-launch-1.0 v4l2src device=/dev/video17 ! video/x-raw,format=YUY2,width=640,height=480, framerate=30/1 ! videoconvert ! autovideosink
 #"
 # Fpr spefic size :
 
-# v4l2-ctl --list-formats-ext -d /dev/video10
+# v4l2-ctl --list-formats-ext -d /dev/video17
