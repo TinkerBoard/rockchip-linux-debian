dc730c918cd7df7e6451c63cf039d09f5cbaeef4
Rock Shen <rock_shen@asus.com>
2022-01-18 20:35:45 +0800

    Mount mmcblkp8 to /boot/
    Since add splash partition, shift boot partition to mmcblkp8.
    
    Change-Id: I350b014493534e10d143a081771b569b23438507
    Signed-off-by: Rock Shen <rock_shen@asus.com>
    
    Former-commit-id: d3f3d0ea2b5ae4553b1b3e9a8930a4b1cdc32cb7

--- overlay/etc/init.d/mountboot.sh
+++ overlay/etc/init.d/mountboot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-MMC=$(lsblk | grep "part /" | grep -v "/[a-z]" | awk -F ' ' '{print $1}' | awk -F 'p8' '{print $1}' | awk -F 'mmc' '{print $2}')
-mount "/dev/mmc${MMC}p7" /boot/
+MMC=$(lsblk | grep "part /" | grep -v "/[a-z]" | awk -F ' ' '{print $1}' | awk -F 'p9' '{print $1}' | awk -F 'mmc' '{print $2}')
+mount "/dev/mmc${MMC}p8" /boot/
