adbc7c6435d0f0ed7457d144a9b6521fb0e94c8f
xuanhao_zhang <xuanhao_zhang@asus.com>
2022-04-20 13:28:44 +0800

    Camera: Fix OV5647 capture failed
    Remove framerate setting
    
    Change-Id: Ifd9b62641ae04484e37bee60d53d49aea862a165
    
    Former-commit-id: 1b467362c1847074238b36faafe21ad51bb9b08d

--- overlay-debug/home/linaro/Desktop/Test_tool/Camera_Display_test_tool_v2/Test_tool/Capture_Test.sh
+++ overlay-debug/home/linaro/Desktop/Test_tool/Camera_Display_test_tool_v2/Test_tool/Capture_Test.sh
@@ -21,9 +21,9 @@ function END()
 Remove_TestResult
 
 echo 60 > /tmp/flicker_mode
+echo -e "Start Preview Test!" | tee -a $ResultFile
 cat /sys/bus/i2c/drivers/imx219/1-0010/name |grep "imx219"
 if [ "$?" == "0" ]; then
-	echo -e "Start Preview Test!" | tee -a $ResultFile
 	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[fmt:SRGGB10_1X10/3280x2464]'
 	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[crop:(0,0)/3280x2464]'
 	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[fmt:YUYV8_2X8/3280x2464]'
@@ -34,16 +34,28 @@ if [ "$?" == "0" ]; then
 	echo -e "Start Capture Test!" | tee -a $ResultFile
 	gst-launch-1.0 v4l2src device=$CSI0 num-buffers=10 ! video/x-raw,format=NV12,width=3280,height=2464 ! jpegenc ! multifilesink location=/tmp/Capture.jpg
 else
-	echo -e "Start Preview Test!" | tee -a $ResultFile
-	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[fmt:SBGGR8_1X8/2592x1944]'
-	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[crop:(0,0)/2592x1944]'
-	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[fmt:YUYV8_2X8/2592x1944]'
-	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[crop:(0,0)/2592x1944]'
-	media-ctl -d /dev/media0 --set-v4l2 '"m00_b_ov5647 1-0036":0[fmt:SBGGR8_1X8/2592x1944]'
-	v4l2-ctl -d /dev/video0 --set-fmt-video=width=2592,height=1944,pixelformat=NV12 --set-crop=top=0,left=0,width=2592,height=1944
-	gst-launch-1.0 v4l2src device=$CSI0 num-buffers=100 ! video/x-raw,format=NV12,width=1280,height=960 ! videoconvert ! autovideosink
-	echo -e "Start Capture Test!" | tee -a $ResultFile
-	gst-launch-1.0 v4l2src device=$CSI0 num-buffers=10 ! video/x-raw,format=NV12,width=2592,height=1944, framerate=30/1 ! jpegenc ! multifilesink location=/tmp/Capture.jpg
+	cat /sys/bus/i2c/drivers/imx477/1-001a/name |grep "imx477"
+	if [ "$?" == "0" ]; then
+		media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[fmt:SRGGB12_1X12/4056x3040]'
+		media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[crop:(0,0)/4056x3040]'
+		media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[fmt:YUYV8_2X8/4056x3040]'
+		media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[crop:(0,0)/4056x3040]'
+		media-ctl -d /dev/media0 --set-v4l2 '"m00_b_imx477 1-001a":0[fmt:SRGGB12_1X12/4056x3040]'
+		v4l2-ctl -d /dev/video0 --set-fmt-video=width=4056,height=3040,pixelformat=NV12 --set-crop=top=0,left=0,width=4056,height=3040
+		gst-launch-1.0 v4l2src device=$CSI0 num-buffers=100 ! video/x-raw,format=NV12,width=4056,height=3040 ! videoconvert ! autovideosink
+		echo -e "Start Capture Test!" | tee -a $ResultFile
+		gst-launch-1.0 v4l2src device=$CSI0 num-buffers=10 ! video/x-raw,format=NV12,width=4056,height=3040 ! jpegenc ! multifilesink location=/tmp/Capture.jpg
+	else
+		media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[fmt:SBGGR8_1X8/2592x1944]'
+		media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[crop:(0,0)/2592x1944]'
+		media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[fmt:YUYV8_2X8/2592x1944]'
+		media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[crop:(0,0)/2592x1944]'
+		media-ctl -d /dev/media0 --set-v4l2 '"m00_b_ov5647 1-0036":0[fmt:SBGGR8_1X8/2592x1944]'
+		v4l2-ctl -d /dev/video0 --set-fmt-video=width=2592,height=1944,pixelformat=NV12 --set-crop=top=0,left=0,width=2592,height=1944
+		gst-launch-1.0 v4l2src device=$CSI0 num-buffers=100 ! video/x-raw,format=NV12,width=1280,height=960 ! videoconvert ! autovideosink
+		echo -e "Start Capture Test!" | tee -a $ResultFile
+		gst-launch-1.0 v4l2src device=$CSI0 num-buffers=10 ! video/x-raw,format=NV12,width=2592,height=1944 ! jpegenc ! multifilesink location=/tmp/Capture.jpg
+	fi
 fi
 
 echo -e "Finished Capture Test!" | tee -a $ResultFile
