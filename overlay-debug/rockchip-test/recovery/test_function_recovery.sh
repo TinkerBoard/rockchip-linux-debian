1d798365d3de8fbdc16a6c4b7b2f0a4f1fa46db2
Caesar Wang <wxt@rock-chips.com>
2022-04-27 19:34:46 +0800

    overlaylay-debug: update recovery
    Change-Id: Ida57ef4cc906a9ab3c440278f48e1bf86b9c70fc
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- overlay-debug/rockchip-test/recovery/test_function_recovery.sh
+++ overlay-debug/rockchip-test/recovery/test_function_recovery.sh
@@ -10,6 +10,14 @@ if [ ! -e "/usr/bin/update" ]; then
 fi
 
 # check log on /userdata/recovery/last_log
+# Reboots the device and wipes the user data partition.  This is
+# sometimes called a "factory reset", which is something of a
+# misnomer because the system partition is not restored to its
+# factory state.
+
 /usr/bin/update
+#/usr/bin/update factory
+#/usr/bin/update ota xxx
+
 sync
 
