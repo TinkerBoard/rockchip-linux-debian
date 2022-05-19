1f429cd2a4b2f9b9f5ce70235509c3bdf1724e02
Caesar Wang <wxt@rock-chips.com>
2022-05-19 15:11:24 +0800

    mk-rootfs-bullseye.sh: add ugly and libav
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: I67280e74f470e0dff5bbda66b2c326de4bef02e9

--- mk-rootfs-bullseye.sh
+++ mk-rootfs-bullseye.sh
@@ -104,6 +104,8 @@ gstreamer1.0-plugins-base-apps qtmultimedia5-examples
 \${APT_INSTALL} /packages/gst-plugins-base1.0/*.deb
 \${APT_INSTALL} /packages/gst-plugins-bad1.0/*.deb
 \${APT_INSTALL} /packages/gst-plugins-good1.0/*.deb
+\${APT_INSTALL} /packages/gst-plugins-ugly1.0/*.deb
+\${APT_INSTALL} /packages/gst-libav1.0/*.deb
 
 #---------Camera---------
 echo -e "\033[36m Install camera.................... \033[0m"
