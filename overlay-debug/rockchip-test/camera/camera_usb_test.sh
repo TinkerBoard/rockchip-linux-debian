382cff7aad0e2f84257ce7a85d2669e096502f76
Caesar Wang <wxt@rock-chips.com>
2022-05-31 18:31:15 +0800

    overlay-debug: update rockchip-test
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: Ie653bcdc00e089e5a1af2491786b8620fb781eec

--- overlay-debug/rockchip-test/camera/camera_usb_test.sh
+++ overlay-debug/rockchip-test/camera/camera_usb_test.sh
@@ -8,7 +8,15 @@ export DISPLAY=:0.0
 #export GST_MPP_JPEGDEC_DEFAULT_FORMAT=NV12
 
 echo "Start UVC Camera M-JPEG Preview!"
+
+COMPATIBLE=$(cat /proc/device-tree/compatible)
+if [[ $COMPATIBLE =~ "rk3588" ]]; then
 gst-launch-1.0 v4l2src device=/dev/video17 ! image/jpeg, width=1280, height=720, framerate=30/1 ! jpegparse ! mppjpegdec ! xvimagesink sync=false
+else
+gst-launch-1.0 v4l2src device=/dev/video9 ! image/jpeg, width=1280, height=720, framerate=30/1 ! jpegparse ! mppjpegdec ! xvimagesink sync=false
+    CHIPNAME="rk3036"
+fi
+COMPATIBLE=${COMPATIBLE#rockchip,}
 
 #     gst-launch-1.0 v4l2src device=/dev/video17 ! video/x-raw,format=YUY2,width=640,height=480, framerate=30/1 ! videoconvert ! autovideosink
 #"
