43328a4255ba3d49e3dff135cac28170a5a28b46
Caesar Wang <wxt@rock-chips.com>
2022-07-02 14:07:42 +0800

    overlay-debug: update rockchip-test
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: I94b3c3cf9c79896ee3f2c06babe9a0035ce65c24

--- overlay-debug/rockchip-test/camera/camera_usb_test.sh
+++ overlay-debug/rockchip-test/camera/camera_usb_test.sh
@@ -8,7 +8,16 @@ export DISPLAY=:0.0
 #export GST_MPP_JPEGDEC_DEFAULT_FORMAT=NV12
 
 echo "Start UVC Camera M-JPEG Preview!"
-gst-launch-1.0 v4l2src device=/dev/video17 ! image/jpeg, width=1280, height=720, framerate=30/1 ! jpegparse ! mppjpegdec ! xvimagesink sync=false
+
+COMPATIBLE=$(cat /proc/device-tree/compatible)
+if [[ $COMPATIBLE =~ "rk3588" ]]; then
+    gst-launch-1.0 v4l2src device=/dev/video20 ! image/jpeg, width=1280, height=720, framerate=30/1 ! jpegparse ! mppjpegdec ! xvimagesink sync=false
+elif [[ $COMPATIBLE =~ "rk3566" && $COMPATIBLE =~ "rk3568" ]]; then
+    gst-launch-1.0 v4l2src device=/dev/video9 ! image/jpeg, width=1280, height=720, framerate=30/1 ! jpegparse ! mppjpegdec ! xvimagesink sync=false
+else
+    gst-launch-1.0 v4l2src device=/dev/video10 ! image/jpeg, width=1280, height=720, framerate=30/1 ! jpegparse ! mppjpegdec ! xvimagesink sync=false
+fi
+COMPATIBLE=${COMPATIBLE#rockchip,}
 
 #     gst-launch-1.0 v4l2src device=/dev/video17 ! video/x-raw,format=YUY2,width=640,height=480, framerate=30/1 ! videoconvert ! autovideosink
 #"
--- overlay-debug/rockchip-test/chromium/test_chromium_with_video.sh
+++ overlay-debug/rockchip-test/chromium/test_chromium_with_video.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -e
 
-export DISPLAY=:0.0 
+export DISPLAY=:0.0
 #export GST_DEBUG=*:5
 #export GST_DEBUG_FILE=/tmp/2.txt
-echo 0x100 > /sys/module/rk_vcodec/parameters/mpp_dev_debug
+
+KERNEL_VERSION=$(cat /proc/version)
+if [[ $KERNEL_VERSION =~ "4.4" ]]; then
+    echo 0x100 > /sys/module/rk_vcodec/parameters/debug
+else
+    echo 0x100 > /sys/module/rk_vcodec/parameters/mpp_dev_debug
+fi
+
 echo performance | tee $(find /sys/ -name *governor) /dev/null || true
 chromium --no-sandbox file:///usr/local/test.mp4
 
