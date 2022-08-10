f629bc22e597ef253e85f7f3a2a50ac73ca72f83
Caesar Wang <wxt@rock-chips.com>
2022-08-10 17:26:47 +0800

    overlay-debug: fixes the gpu tests on rockchip-test
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: I0183e52a1fd04ff52142faacecee4a2548cca7d2

--- overlay-debug/rockchip-test/gpu/test_offscreen_glmark2.sh
+++ overlay-debug/rockchip-test/gpu/test_offscreen_glmark2.sh
@@ -9,7 +9,7 @@ elif [[  "$1" == "rk3328"  ]]; then
 	glmark2-es2 --off-screen
 
 elif [[  "$1" == "rk3399"  ]]; then
-	taskset -c 4-5 glamrk2-es2 --off-screen
+	taskset -c 4-5 glmark2-es2 --off-screen
 
 elif [[  "$1" == "rk3399pro"  ]]; then
 	taskset -c 4-5 glmark2-es2 --off-screen
