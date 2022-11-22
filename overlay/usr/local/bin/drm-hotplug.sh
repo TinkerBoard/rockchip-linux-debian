005885ebc8fa1a829ab240a8aee908b39a4b857a
bross1_kuo <bross1_kuo@asus.com>
2022-11-22 15:25:39 +0800

    display: HDMI/DP: use sudo command directly when use xrandr to change resolution
    also modify parsing xrandr information rule and scrpit mechanism to fix follow bug
    
    1.HDMI/DP connect to a monitor whose preferred resolution is different with previous monitor connected, will blank screen
    
    2.connect with HDMI monitor and DSI panel at the same time, remove HDMI monitor, DSI panel keep extension mode layout
    
    3.connect with DSI panel, then plug HDMI/DP monitor, HDMI/DP monitor won't turn on automatic, need to monitor settings to turn on
    
    4.display blank if plug HDMI/DP cable after system boot
    
    Change-Id: Ib7056efb35ff94eea23890926105d8656e4ef053
    Signed-off-by: bross1_kuo <bross1_kuo@asus.com>
    
    Former-commit-id: 1532beec12f681a81fed8cd46f057e94d671d698

--- overlay/usr/local/bin/drm-hotplug.sh
+++ overlay/usr/local/bin/drm-hotplug.sh
@@ -13,7 +13,7 @@ DP_XRANDR_CONFIG="/boot/display/dp/xrandr.cfg"
 
 HDMI_HOTPLUG_CONFIG="/boot/display/hdmi/hdmi_plug_flag.cfg"
 DP_HOTPLUG_CONFIG="/boot/display/dp/dp_plug_flag.cfg"
-
+DISPLAY_BOOT_FLAG="/boot/display/display_boot_flag.cfg"
 
 HDMI_MODES_NODE="$HDMI_SYS/modes"
 DP_MODES_NODE="$DP_SYS/modes"
@@ -27,29 +27,29 @@ dp_status=$(cat /sys/class/drm/card0-DP-1/status)
 #Save resolution if the external display is disconnected
 #HDMI
 if [ $hdmi_status = "disconnected" ]; then
-	#if [ -f $HDMI_HOTPLUG_CONFIG ]; then
-	#	if [ "$(cat $HDMI_HOTPLUG_CONFIG)" != "Plug_Out" ]; then
-	#		if [ "$(cat $HDMI_MODES_NODE)" != "" ]; then
-	#			HDMI_SAVE_MODE=$(su $user -c xrandr | grep HDMI | awk '{print$3}' | awk -F '+' '{print$1}')
-	#			su $user -c "echo $HDMI_SAVE_MODE" > $HDMI_XRANDR_CONFIG
-	#		fi
-	#	fi
-	#fi
-	sudo -u $user xrandr --output $HDMI --off
+	if [ -f $HDMI_HOTPLUG_CONFIG ]; then
+		if [ "$(cat $HDMI_HOTPLUG_CONFIG)" != "Plug_Out" ]; then
+			#if [ "$(cat $HDMI_MODES_NODE)" != "" ]; then
+				HDMI_SAVE_MODE=$(su $user -c xrandr | grep HDMI | awk '{print$3}' | awk -F '+' '{print$1}')
+				su $user -c "echo $HDMI_SAVE_MODE" > $HDMI_XRANDR_CONFIG
+			#fi
+		fi
+	fi
+	sudo xrandr --output $HDMI --off
 	su $user -c "echo Plug_Out" > $HDMI_HOTPLUG_CONFIG
 fi
 
 #DP
 if [ $dp_status = "disconnected" ]; then
-	#if [ -f $DP_HOTPLUG_CONFIG ]; then
-	#	if [ "$(cat $DP_HOTPLUG_CONFIG)" != "Plug_Out" ]; then
-	#		if [ "$(cat $DP_MODES_NODE)" != "" ]; then
-	#			DP_SAVE_MODE=$(su $user -c xrandr | grep DP | awk '{print$4}' | awk -F '+' '{print$1}')
-	#			su $user -c "echo $DP_SAVE_MODE" > $DP_XRANDR_CONFIG
-	#		fi
-	#	fi
-	#fi
-	sudo -u $user xrandr --output $DP --off
+	if [ -f $DP_HOTPLUG_CONFIG ]; then
+		if [ "$(cat $DP_HOTPLUG_CONFIG)" != "Plug_Out" ]; then
+			#if [ "$(cat $DP_MODES_NODE)" != "" ]; then
+				DP_SAVE_MODE=$(su $user -c xrandr | grep DP | awk '{print$4}' | awk -F '+' '{print$1}')
+				su $user -c "echo $DP_SAVE_MODE" > $DP_XRANDR_CONFIG
+			#fi
+		fi
+	fi
+	sudo xrandr --output $DP --off
 	su $user -c "echo Plug_Out" > $DP_HOTPLUG_CONFIG
 fi
 
@@ -59,22 +59,26 @@ fi
 if [ $hdmi_status = "connected" ]; then
 	if [ -f $HDMI_HOTPLUG_CONFIG ]; then
 		if [ "$(cat $HDMI_HOTPLUG_CONFIG)" = "Plug_Out" ]; then
-			if [ "$(cat $HDMI_MODES_NODE)" != "" ]; then
-				HDMI_SAVE_MODE=$(su $user -c xrandr | grep HDMI | awk '{print$3}' | awk -F '+' '{print$1}')
-				su $user -c "echo $HDMI_SAVE_MODE" > $HDMI_XRANDR_CONFIG
-			fi
+			#if [ "$(cat $HDMI_MODES_NODE)" != "" ]; then
+			#	HDMI_SAVE_MODE=$(su $user -c xrandr | grep HDMI | awk '{print$3}' | awk -F '+' '{print$1}')
+			#	su $user -c "echo $HDMI_SAVE_MODE" > $HDMI_XRANDR_CONFIG
+			#fi
 			if [ -f $HDMI_XRANDR_CONFIG ]; then
 				if grep -q $(cat $HDMI_XRANDR_CONFIG) $HDMI_MODES_NODE; then
-					sudo -u $user xrandr --output $HDMI --mode $(cat $HDMI_XRANDR_CONFIG)
+					sudo xrandr --output $HDMI --mode $(cat $HDMI_XRANDR_CONFIG)
 				else
-					sudo -u $user xrandr --output $HDMI --auto
+					sudo xrandr --output $HDMI --auto
 				fi
 			else
-				sudo -u $user xrandr --output $HDMI --auto
+				sudo xrandr --output $HDMI --auto
 			fi
 		else
-			if [ "$(cat $HDMI_MODE_NODE)" = "" ]; then
-				sudo -u $user xrandr --output $HDMI --auto
+			if [ "$(cat $HDMI_XRANDR_CONFIG)" = "" ]; then
+				if [ "$(cat $DISPLAY_BOOT_FLAG)" = "1" ]; then
+					su $user -c "echo 0" > $DISPLAY_BOOT_FLAG
+				else
+					sudo xrandr --output $HDMI --auto
+				fi
 			fi
 		fi
 	fi
@@ -85,22 +89,26 @@ fi
 if [ $dp_status = "connected" ]; then
 	if [ -f $DP_HOTPLUG_CONFIG ]; then
 		if [ "$(cat $DP_HOTPLUG_CONFIG)" = "Plug_Out" ]; then
-			if [ "$(cat $DP_MODES_NODE)" != "" ]; then
-				DP_SAVE_MODE=$(su $user -c xrandr | grep DP | awk '{print$4}' | awk -F '+' '{print$1}')
-				su $user -c "echo $DP_SAVE_MODE" > $DP_XRANDR_CONFIG
-			fi
+			#if [ "$(cat $DP_MODES_NODE)" != "" ]; then
+			#	DP_SAVE_MODE=$(su $user -c xrandr | grep DP | awk '{print$4}' | awk -F '+' '{print$1}')
+			#	su $user -c "echo $DP_SAVE_MODE" > $DP_XRANDR_CONFIG
+			#fi
 			if [ -f $DP_XRANDR_CONFIG ]; then
 				if grep -q $(cat $DP_XRANDR_CONFIG) $DP_MODES_NODE; then
-					sudo -u $user xrandr --output $DP --mode $(cat $DP_XRANDR_CONFIG)
+					sudo xrandr --output $DP --mode $(cat $DP_XRANDR_CONFIG)
 				else
-					sudo -u $user xrandr --output $DP --auto
+					sudo xrandr --output $DP --auto
 				fi
 			else
-				sudo -u $user xrandr --output $DP --auto
+				sudo xrandr --output $DP --auto
 			fi
 		else
-			if [ "$(cat $DP_MODE_NODE)" = "" ]; then
-				sudo -u $user xrandr --output $DP --auto
+			if [ "$(cat $DP_XRANDR_CONFIG)" = "" ]; then
+				if [ "$(cat $DISPLAY_BOOT_FLAG)" = "1" ]; then
+					su $user -c "echo 0" > $DISPLAY_BOOT_FLAG
+				else
+					sudo xrandr --output $DP --auto
+				fi
 			fi
 		fi
 	fi
