696251f1aaecb46ba1e327f4843f53aab54ed58c
yi-hsin_hung <yi-hsin_hung@asus.com>
2022-12-14 16:54:25 +0800

    Add thunar-volman for storage auto mount
    Change-Id: I65ec3268000522b6952296ca0e01a08e7e250fe9
    Signed-off-by: yi-hsin_hung <yi-hsin_hung@asus.com>
    
    Former-commit-id: a16ad170879708410bf04c82d3eab9175f609802

--- mk-rootfs-bullseye.sh
+++ mk-rootfs-bullseye.sh
@@ -209,6 +209,12 @@ source ~/.bashrc
 #\${APT_INSTALL} xfce4
 #ln -sf /usr/bin/startxfce4 /etc/alternatives/x-session-manager
 
+#---------------Install thunar-volman and auto mount storage start--------------
+\${APT_INSTALL} thunar-volman
+cp /etc/ASUS/thunar-volman.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/thunar-volman.xml
+
+#---------------Install thunar-volman and auto mount storage end--------------
+
 # HACK to disable the kernel logo on bootup
 #sed -i "/exit 0/i \ echo 3 > /sys/class/graphics/fb0/blank" /etc/rc.local
 
