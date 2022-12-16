a300478a55936183db74d6b424742f3d895fdcaf
Tzuhsuan_Chen <Tzuhsuan_Chen@asus.com>
2022-12-16 09:44:07 +0800

    leds: Support led control for emmc or sdcard read/write.
    Change-Id: I89c64b6564e6f3881cfda28a97d4209b5390f797
    
    Former-commit-id: 8ac39e39d223ae5cb8c9336b195fcb4cf456ac94

--- overlay/etc/init.d/rockchip.sh
+++ overlay/etc/init.d/rockchip.sh
@@ -95,6 +95,18 @@ fi
 COMPATIBLE=${COMPATIBLE#rockchip,}
 BOARDNAME=${COMPATIBLE%%rockchip,*}
 
+# set act-led trigger function
+cmdline=$(cat /proc/cmdline)
+storage=`echo $cmdline|awk '{print match($0,"storagemedia=emmc")}'`;
+
+if [ $storage -gt 0 ]; then
+    #emmc
+    echo mmc0 > /sys/class/leds/act-led/trigger
+else
+    #sdcard
+    echo mmc1 > /sys/class/leds/act-led/trigger
+fi
+
 # first boot configure
 if [ ! -e "/usr/local/first_boot_flag" ] ;
 then
