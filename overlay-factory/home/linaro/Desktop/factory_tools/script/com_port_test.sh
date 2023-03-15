fb775e1389071e9a35d5dcf96631903c0523d3b7
RyanLY_Wang <RyanLY_Wang@asus.com>
2023-03-15 17:23:30 +0800

    [UART] Update script for UART factory test.
    Change-Id: Iddae839e8a1fa234c3a6c1381b09355b815e5890
    
    Former-commit-id: 0983995a0a01a3240f355f93e2a460839919a5ef

--- overlay-factory/home/linaro/Desktop/factory_tools/script/com_port_test.sh
+++ overlay-factory/home/linaro/Desktop/factory_tools/script/com_port_test.sh
@@ -1,97 +1,73 @@
 #!/bin/bash
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-
-PROJECT="Tinker3"
-version=1.0.0
-
-COM1to2_result=0
-COM2to1_result=0
-COM1to1_result=0
-COM2to2=0
+COM1to2=0
+COM2to1=0
+COM1to1=0
+protocol=0
+flow_control=0
 
 BINARY_PATH=$SCRIPTPATH'/../bin'
 TEST_BINARY_PATH=$BINARY_PATH'/com_port_test'
 SWITCH_SCRIPT_PATH=$SCRIPTPATH'/switch_com3_protocol.sh'
 
-select_test_item()
-{
-	echo "============================================"
-	echo "     $PROJECT COM Port Test v_$version"
-	echo "============================================"
-	echo
-	echo " 1. COM1 RS232 self test"
-	echo " 2. COM2 RS232 self test"
-	echo " 3. COM1 RS232, COM2 RS232 loop test"
-    echo " 4. COM1 RS232, COM2 RS232 loop test with flow control"
-	echo " 5. COM1 RS232, COM3 RS232 loop test"
-	echo " 6. COM1 RS232, COM3 RS422 loop test"
-	echo " 7. COM1 RS232, COM3 RS485 loop test"
+if [ $# == 1 ]; then
+    COM1=$1
+    COM2="none"
+    testmode="one_port"
+elif [ $# == 4 ]; then
+    COM1=$1
+    COM2=$2
+    protocol=$3
+    flow_control=$4
+    testmode="two_port"
+else
+    echo "Wrong parameter amount, please check the following format."
+    echo "1. comport_test.sh [COM1] [COM2] [PROTOCOL] [FLOW_CONTROL]"
+    echo "2. comport_test.sh [COM1]"
+    exit 1
+fi
 
-	read -p "Select test case: " test_item
-}
+TEST_SCRIPT=$TEST_BINARY_PATH' -p '$COM1' '$COM2
 
-select_test_item
-echo
+if [ "$flow_control" == "1" ]; then
+    TEST_SCRIPT="$TEST_BINARY_PATH -p $COM1 $COM2 -f"
+fi
 
-case $test_item in
-	1)
-		COM1to1_result=`$TEST_BINARY_PATH -p /dev/ttyS0 none -3`
-        ;;
-	2)
-		COM1to1_result=`$TEST_BINARY_PATH -p /dev/ttyS8 none -3`
-        ;;
-	3)
-		COM1to2_result=`$TEST_BINARY_PATH -p /dev/ttyS0 /dev/ttyS8 -1`
-        sleep 1
-        COM2to1_result=`$TEST_BINARY_PATH -p /dev/ttyS0 /dev/ttyS8 -2`
-        ;;
-	4)
-		COM1to2_result=`$TEST_BINARY_PATH -p /dev/ttyS0 /dev/ttyS8 -1 -f`
-        COM2to1_result=`$TEST_BINARY_PATH -p /dev/ttyS0 /dev/ttyS8 -2 -f`
-        ;;
-	5)
-        . $SWITCH_SCRIPT_PATH 232
-		COM1to2_result=`$TEST_BINARY_PATH -p /dev/ttyS0 /dev/ttyS3 -1`
-        sleep 1
-        COM2to1_result=`$TEST_BINARY_PATH -p /dev/ttyS0 /dev/ttyS3 -2`
-        ;;
-	6)
-        . $SWITCH_SCRIPT_PATH 422
-		COM1to2_result=`$TEST_BINARY_PATH -p /dev/ttyS0 /dev/ttyS3 -1`
-        sleep 1
-        COM2to1_result=`$TEST_BINARY_PATH -p /dev/ttyS0 /dev/ttyS3 -2`
-        ;;
-	7)
-        . $SWITCH_SCRIPT_PATH 485
-		COM1to2_result=`$TEST_BINARY_PATH -p /dev/ttyS0 /dev/ttyS3 -1`
-        sleep 1
-        COM2to1_result=`$TEST_BINARY_PATH -p /dev/ttyS0 /dev/ttyS3 -2`
-        ;;
-	*)
-        echo "Select error."
-		;;
-esac
-
-if [ "$test_item" == "1" -o "$test_item" == "2" ]
-then
-    if [ "$COM1to1_result" == "PASS" ]
+if [ "$testmode" == "one_port" ]; then
+    COM1to1=`$TEST_SCRIPT -3`
+    if [ "$COM1to1" == "PASS" ]
     then
         echo "TEST PASS"
     else
         echo "=============FAIL============="
-        echo "Test result : $COM1to1_result"
+        echo "Test result : $COM1to1"
+        echo "=============================="
+    fi
+elif [ "$testmode" == "two_port" ]; then
+    if [ "$protocol" == "232" ]; then
+        . $SWITCH_SCRIPT_PATH 232
+    elif [ "$protocol" == "422" ]; then
+        . $SWITCH_SCRIPT_PATH 422
+    elif [ "$protocol" == "485" ]; then
+        . $SWITCH_SCRIPT_PATH 485
+    fi
+
+    COM1to2=`$TEST_SCRIPT -1`
+    sleep 1
+    COM2to1=`$TEST_SCRIPT -2`
+
+    if [ "$COM1to2" == "PASS" -a "$COM2to1" == "PASS" ]
+    then
+        echo "TEST PASS"
+    else
+        echo "=============FAIL============="
+        echo "Test result 1 : $COM1to2"
+        echo "Test result 2 : $COM2to1"
         echo "=============================="
     fi
 else
-    if [ "$COM1to2_result" == "PASS" -a "$COM2to1_result" == "PASS" ]
-    then
-        echo "TEST PASS"
-    else
-        echo "=============FAIL============="
-        echo "Test result 1 : $COM1to2_result"
-        echo "Test result 2 : $COM2to1_result"
-        echo "=============================="
-
-    fi
+    echo "Wrong testmode option, please check the following information."
+    exit 1
 fi
+
