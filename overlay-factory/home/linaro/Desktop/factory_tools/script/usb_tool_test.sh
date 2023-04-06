a5ae19be96d99cbb8a663628ee7f3576b73a2705
tzuwen_chang <TzuWen_Chang@asus.com>
2023-04-06 15:28:49 +0800

    usb: upgrade usb test script and tool for factory
    version: 1.47
    Change-Id: I384553c7c4ce5971582d65466297d2aeae73faa1
    
    Former-commit-id: 617fce64372a79cafdda8427f78659abededa640

--- overlay-factory/home/linaro/Desktop/factory_tools/script/usb_tool_test.sh
+++ overlay-factory/home/linaro/Desktop/factory_tools/script/usb_tool_test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-	echo "usage: sudo ./usb_tool_test.sh <u2 port number> <u3 port number> <test times>"
+if [ $# -ne 4 ]; then
+	echo "usage: sudo ./usb_tool_test.sh <u2 port number> <u3 port number> <typeC port number> <test times>"
 	exit -1
 fi
 
 u2port=$1
 u3port=$2
-count=$3
+tcport=$3
+count=$4
 
 cardCount=$(lsusb|grep "iCreate Technologies"|wc -l)
 if [ "$cardCount" != "$u2port" ]; then
@@ -17,30 +18,15 @@ fi
 
 for((i=1; i<=count; i++))
 do
-	sudo UsbTool /tdc -hc $u2port -lc $u2port -fc $u2port -sc $u3port -nu30s 0 -d3f 0 -ccst 1\
+	sudo UsbTool /tdc -hc $u2port -sc $u3port -bhc $tcport -bsc $tcport -nu30s 0 -d3f 0 -ccst 1\
 		> /var/log/usb_test_card.log
 
 	if [ "$?" == "0" ]; then
 		if [ "$i" != "$count" ]; then
-			sleep 5
+			sleep 2
 		fi
 	else
-		grep USB31S /var/log/usb_test_card.log|grep Fail > /dev/null 2>&1
-		if [ "$?" == "0" ]; then
-			echo "FAIL, USB31S test fail"
-		fi
-		grep USB31H /var/log/usb_test_card.log|grep Fail > /dev/null 2>&1
-		if [ "$?" == "0" ]; then
-			echo "FAIL, USB31H test fail"
-		fi
-		grep USB30S /var/log/usb_test_card.log|grep Fail > /dev/null 2>&1
-		if [ "$?" == "0" ]; then
-			echo "FAIL, USB30S test fail"
-		fi
-		grep USB30H /var/log/usb_test_card.log|grep Pass > /dev/null 2>&1
-		if [ "$?" == "1" ]; then
-			echo "FAIL, USB30H test fail"
-		fi
+		echo "FAIL"
 		exit -1
 	fi
 done
