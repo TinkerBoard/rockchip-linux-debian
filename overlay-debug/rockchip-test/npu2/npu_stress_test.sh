bafc23f515f3db0793e4de0c2ef269a32bddc94b
Caesar Wang <wxt@rock-chips.com>
2023-05-11 10:04:59 +0800

    rknpu2: update rknpu2 for rk3562
    Built from rk3562_rknn_stress_test_v0.5_20230510
    
    Change-Id: I0aef2aa6574b75a887a83278c09decfaa90b54da
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    
    Former-commit-id: 627210a6129e2b79d846b13cb73298ac8eae9279

--- overlay-debug/rockchip-test/npu2/npu_stress_test.sh
+++ overlay-debug/rockchip-test/npu2/npu_stress_test.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 COMPATIBLE=$(cat /proc/device-tree/compatible)
-
+BIN=rknn_stress_test
+TOTAL_TIME=10
 
 while true
 do
 if [[ $COMPATIBLE =~ "rk3588" ]]; then
     rknn_common_test /usr/share/model/RK3588/mobilenet_v1.rknn /usr/share/model/dog_224x224.jpg 10
 elif [[ $COMPATIBLE =~ "rk3562" ]]; then
-    ln -sf /usr/share/model/RK3562/mobilenet_v2 /data/mobilenet_v2
-    ln -sf /usr/share/model/RK3562/resnet_50 /data/resnet_50
-    ln -sf /usr/share/model/RK3562/vgg16_max_pool /data/vgg16_max_pool
-    rknn_stress_test /usr/share/model/RK3562/mobilenet_v2/mobilenet_v2_fp16.cfg 10
-    rknn_stress_test /usr/share/model/RK3562/resnet_50/resnet_50_fp16.cfg 10
-    rknn_stress_test /usr/share/model/RK3562/vgg16_max_pool/vgg16_max_pool_fp16.cfg 10
+    ln -rsf /usr/share/model/RK3562/data/* /data/
+    $BIN /usr/share/model/RK3562/cfg/mobilenet_v2_i8.cfg $TOTAL_TIME
+    $BIN /usr/share/model/RK3562/cfg/mobilenet_v2_fp16.cfg $TOTAL_TIME
+    $BIN /usr/share/model/RK3562/cfg/resnet50_i8.cfg $TOTAL_TIME
+    $BIN /usr/share/model/RK3562/cfg/resnet50_fp16.cfg $TOTAL_TIME
+    $BIN /usr/share/model/RK3562/cfg/vgg16_max_pool_i8.cfg $TOTAL_TIME
+    $BIN /usr/share/model/RK3562/cfg/vgg16_max_pool_fp16.cfg $TOTAL_TIME
 else
     rknn_common_test /usr/share/model/RK356X/mobilenet_v1.rknn /usr/share/model/dog_224x224.jpg 10
 fi
