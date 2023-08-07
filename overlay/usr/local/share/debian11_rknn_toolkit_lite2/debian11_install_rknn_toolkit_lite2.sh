35b615ea5baa908eefc8ecbe5d8819d8b48240e0
pomelo_hsieh <pomelo_hsieh@asus.com>
2023-08-07 16:46:31 +0800

    Change install script for rknn_toolkit_lite2 version 1.5.0.
    Change-Id: I06fc738a3316eee9864a6b57df88e620f4d1e7ea
    Signed-off-by: pomelo_hsieh <pomelo_hsieh@asus.com>
    
    Former-commit-id: b800adae4d396307acdbbdc4f09848d190ea51dc

--- overlay/usr/local/share/debian11_rknn_toolkit_lite2/debian11_install_rknn_toolkit_lite2.sh
+++ overlay/usr/local/share/debian11_rknn_toolkit_lite2/debian11_install_rknn_toolkit_lite2.sh
@@ -8,7 +8,7 @@ function pause(){
 sudo apt-get update
 sudo apt-get install -y python3 python3-dev python3-pip gcc g++
 pip3 install pip --upgrade
-pip3 install --user /usr/local/share/debian11_rknn_toolkit_lite2/rknn_toolkit_lite2-1.4.0-cp39-cp39-linux_aarch64.whl
+pip3 install --user /usr/local/share/debian11_rknn_toolkit_lite2/rknn_toolkit_lite2-1.5.0-cp39-cp39-linux_aarch64.whl
 pip3 install --user opencv-python opencv-contrib-python
 sudo apt-get autoremove -y
 
