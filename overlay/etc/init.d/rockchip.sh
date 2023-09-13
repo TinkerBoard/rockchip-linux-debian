1bf29f198533081ab26f25f69871bee0542bf9e5
Tzuhsuan_Chen <Tzuhsuan_Chen@asus.com>
2023-09-13 10:21:38 +0800

    leds: Check if the act led trigger file exists.
    Change-Id: Ifa4c1ad07f63b80ee557564de348a15dbfeb2e9a
    
    Former-commit-id: 00a54fbb837d15b441399987aee757cbdbbab40f

--- overlay/etc/init.d/rockchip.sh
+++ overlay/etc/init.d/rockchip.sh
@@ -222,14 +222,15 @@ hwclock --systohc
 /etc/network/ethernet_wol.sh
 
 # set act-led trigger function
-cmdline=$(cat /proc/cmdline)
-storage=`echo $cmdline|awk '{print match($0,"storagemedia=emmc")}'`;
+if [ -e "/sys/class/leds/act-led/trigger" ]; then
+    cmdline=$(cat /proc/cmdline)
+    storage=`echo $cmdline|awk '{print match($0,"storagemedia=emmc")}'`;
 
-if [ $storage -gt 0 ]; then
-    #emmc
-    echo mmc0 > /sys/class/leds/act-led/trigger
-else
-    #sdcard
-    echo mmc1 > /sys/class/leds/act-led/trigger
+    if [ $storage -gt 0 ]; then
+        #emmc
+        echo mmc0 > /sys/class/leds/act-led/trigger
+    else
+        #sdcard
+        echo mmc1 > /sys/class/leds/act-led/trigger
+    fi
 fi
-
