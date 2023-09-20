233430c456b116e6826f9234b85e9e0bea843a5f
kengyen_chen <kengyen_chen@asus.com>
2023-09-20 09:57:02 +0800

    Audio: change service type and config audio default output at boot time for TB3 & TB3N
    Change-Id: Ifbd1b7167fc3c9694506b149ac6c9e3ad41ac83b
    
    Former-commit-id: 3ca7cd9c8cbb44fefc22d1d1f5b94def9a4afb07

--- overlay/usr/local/bin/switch_sound_device_boot.sh
+++ overlay/usr/local/bin/switch_sound_device_boot.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 # Config audio output devices at boot time
 hdmi_status=$(cat /sys/class/drm/card0-HDMI-A-1/status)
-jack_status=$(cat /sys/class/extcon/extcon4/cable.1/state)
+jack_tb3n_status=$(cat /sys/class/extcon/extcon4/cable.1/state)
+jack_tb3_status=$(cat /sys/class/extcon/extcon3/cable.1/state)
 
-if [ $hdmi_status = "connected" ] && [ $jack_status = 0 ];
+if [ $jack_tb3n_status = 0 ] || [ $jack_tb3_status = 0 ];
 then
-	/bin/bash /etc/pulse/switch_sound_device.sh "alsa_output.platform-hdmi-sound.stereo-fallback"
+	if [ $hdmi_status = "connected" ];
+	then
+		echo "HDMI is connected, set default sound card to HDMI"
+		/bin/bash /etc/pulse/switch_sound_device.sh "alsa_output.platform-hdmi-sound.stereo-fallback"
+	fi
 else
-	/bin/bash /etc/pulse/switch_sound_device.sh "alsa_output.platform-rk809-sound.HiFi__hw_rockchiprk809__sink"
+	echo "audio jack is connected, set default sound card to RK809"
+	/bin/bash  /etc/pulse/switch_sound_device.sh "alsa_output.platform-rk809-sound.HiFi__hw_rockchiprk809__sink"
 fi
